38186ed2743d4f188e4131c4e4f13ce1afdaeb95
Kar_M <kardmode@gmail.com>
2016-12-27 19:37:53 +0400

    bom setup

---
+++ erpnext/stock/report/import_document/import_document.js
@@ -0,0 +1,29 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Import Document"] = {
+	"filters": [
+		{
+			"fieldname": "item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Item"
+		},
+		{
+			"fieldname": "warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Warehouse",
+			"get_query": function() {
+				return{
+					filters: [["Warehouse", "disabled", "=", 0],
+					["Warehouse", "parent_warehouse", "=", "Import Docs - SLI"]],
+				}
+					
+				
+			},
+		},
+	]
+}
